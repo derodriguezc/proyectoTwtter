c9c92425b0c5ea649cd9dd7abf03ba33b9522846
Daniel Rodriguez <derodriguezc7@gmail.com>
2021-08-04 21:00:29 -0400

    Hito2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,36 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
     has_many :tweets, dependent: :destroy
-    has_many :likes
+    has_many :likes, dependent: :destroy
     has_many :liked_tweets, :through => :likes, :source => :tweet
+    has_many :friends, dependent: :destroy
+
+  def is_following?(friend_id)
+    self.friends.where(:friend_id => friend_id).exists?
+  end
+
+  def arr_friends_id
+    friends.map(&:friend_id)
+  end
+
+  def arr_friends_id_and_me
+    friends.map(&:friend_id).push(id)
+  end
+
+  def friend_count
+    friends.count
+  end
+
+  def tweets_count
+    tweets.count
+  end
+
+  def likes_give_it
+    likes.count
+  end
+
+  def retweet_give_it
+    tweets.where.not(rt_ref: nil).count
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+
+  ActiveAdmin.routes(self)
   resources :tweets do
     post 'likes', to: 'tweets#likes'
     post 'retweet', to: 'tweets#retweet'
   end
   devise_for :users
   get 'home/index'
+  #get 'users/follow'
+  post 'follow/:friend_id', to: 'users#follow', as: 'users_follow'
+
+  get 'all_tweets', to: 'home#all_tweets', as: 'all_tweets'
 
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
