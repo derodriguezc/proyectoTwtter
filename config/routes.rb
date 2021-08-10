92506921a45193067aaa88c25a4fad16e887a22f
Daniel Rodriguez <derodriguezc7@gmail.com>
2021-08-09 20:36:12 -0400

    Rest

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   ActiveAdmin.routes(self)
   resources :tweets do
     post 'likes', to: 'tweets#likes'
@@ -11,7 +10,12 @@ Rails.application.routes.draw do
   post 'follow/:friend_id', to: 'users#follow', as: 'users_follow'
 
   get 'all_tweets', to: 'home#all_tweets', as: 'all_tweets'
-
+  #get 'api/news'
+  scope '/api' do
+    get '/news', to: 'api#news', as: 'api_news'
+    get '/:date1/:date2', to: 'api#tweets_by_date', as: 'api_tweets_by_date'
+    post '/tweets', to: 'api#create_tweet', as: 'api_create_tweet'
+  end
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
